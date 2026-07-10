e973f4d09ea7d1459b4db6eac4d8f14a59a8b512
Ali Hasnain <alihasnain2k19@gmail.com>
2026-07-10 23:16:31 +0500

    Add Week 4 Day 1 NumPy materials
    Adds the Day 1 NumPy lecture, practice, and assignment notebooks, plus a small calculator helper module and test script. Also updates `.gitignore` to exclude Week 4 data and later day folders.

---
+++ Week-04/Day-01/MyFunctions/calculator.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    """
+    This function adds two values and return addition result
+    - You have to provide values for x and y
+    """
+    return x + y
+
+def sub(x,y):
+    """
+    This function substract two values and return Substraction result
+    - You have to provide values for x and y
+    """
+    return x - y
+
+def multiply(x,y):
+    """
+    This function multiple two values and return multiplication result
+    - You have to provide values for x and y
+    """
+    return x * y
+
---
+++ Week-04/Day-01/test.py
@@ -0,0 +1,17 @@
+# # import calculator
+
+
+# result_1=calculator.add(12,2)
+# print(result_1)
+
+# result_2 = calculator.sub(12,2)
+# print(f"Result of Sub {result_2}")
+
+# result_3 = calculator.multiply(4,3)
+# print(f"Result of Multiply {result_3}")
+
+
+from MyFunctions.calculator import *
+
+result = multiply(12,2)
+print(result)
